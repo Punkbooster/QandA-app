26c90032e3a3ee7826c69df48cbf98b2bd38c216
Punkbooster <2morrow2late@rambler.ru>
2016-05-02 19:55:14 +0200

    Add new action

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml'
 gem 'rubocop'
 gem 'bootstrap-sass'
+gem 'simple_form'
 
 group :development, :test do
   gem 'byebug'
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,43 @@
 class EventsController < ApplicationController
+  before_action :find_event, only: [:show, :edit, :destroy, :update]
   def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+
+  end
+
+  def create
+    @event = Event.new(params.require(:event).permit(:title, :description))
+
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+
   end
 end
