b03d174c577c2f78ce1b0b2b1d1ec3020bf56653
Punkbooster <2morrow2late@rambler.ru>
2016-05-03 23:00:45 +0200

    Add create/destroy methods & question partials

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,18 @@
 class QuestionsController < ApplicationController
+  def create
+    @event = Event.find(params[:event_id])
+    @question = @event.questions.create(params[:question].permit(:content))
+    if @question.save
+			redirect_to event_path(@event)
+		else
+			render 'events/show'
+		end
+  end
+
+  def destroy
+    @event = Event.find(params[:event_id])
+    @question = @event.questions.find(params[:id])
+    @question.destroy
+		redirect_to event_path(@event)
+	end
 end
