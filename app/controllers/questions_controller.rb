8dab190c984677a293ffaa8065d1f8a0a2cc3e35
Punkbooster <2morrow2late@rambler.ru>
2016-05-04 12:41:22 +0200

    Add question edit/update actions & extract private methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,43 @@
 class QuestionsController < ApplicationController
+  before_action :find_event, only: [:create, :edit, :update, :destroy]
+  before_action :find_question, only: [:edit, :update, :destroy]
+
   def create
-    @event = Event.find(params[:event_id])
-    @question = @event.questions.create(params[:question].permit(:content))
+    @question = @event.questions.create(question_params)
     if @question.save
-			redirect_to event_path(@event)
-		else
-			render 'events/show'
-		end
+      redirect_to event_path(@event)
+    else
+      render 'events/show'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to event_path(@event)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    @event = Event.find(params[:event_id])
-    @question = @event.questions.find(params[:id])
     @question.destroy
-		redirect_to event_path(@event)
+    redirect_to event_path(@event)
 	end
+
+  private
+
+  def find_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def find_question
+    @question = @event.questions.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:content)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :event
+
+  validates :content, presence: true
 end
